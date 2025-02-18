3c3d0946d46df29c1d3b10df86498877aed04b92
Vincent La <vincentla20877@gmail.com>
2025-02-17 22:21:12 -0500

    various submit scripts + more audio

---
+++ submit_scripts/talkshow_french/submit.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#SBATCH --job-name=a2p_french                     # sets the job name
+#SBATCH --output=a2p_french.%j                    # indicates a file to redirect STDOUT to; %j is the jobid. If set, must be set to a file instead of a directory or else submission will fail.
+#SBATCH --error=a2p_french.%j                     # indicates a file to redirect STDERR to; %j is the jobid. If set, must be set to a file instead of a directory or else submission will fail.
+#SBATCH --time=08:00:00                            # how long you would like your job to run; format=hh:mm:ss
+
+#SBATCH --partition=scavenger
+#SBATCH --qos=scavenger                            # set QOS, this will determine what resources can be requested
+#SBATCH --account=scavenger
+#SBATCH --gres=gpu:rtxa5000:1
+
+#SBATCH --nodes=1                                  # number of nodes to allocate for your job
+#SBATCH --ntasks=1                                             
+#SBATCH --ntasks-per-node=1                                     
+#SBATCH --mem=64gb                                 # (cpu) memory required by job; if unit is not specified MB will be assumed
+
+module load cuda
+module load ffmpeg
+source ~/.bashrc
+micromamba activate a2p_env
+
+python inference.py --person_id PXB184 \
+                    --input_audio audio/french_1_trimmed.wav \
+                    --output_dir outputs/talkshow_french/PXB184 \
+                    --num_samples 3
+
+python inference.py --person_id GQS883 \
+                    --input_audio audio/french_1_trimmed.wav \
+                    --output_dir outputs/talkshow_french/GQS883 \
+                    --num_samples 3
+
+python inference.py --person_id RLW104 \
+                    --input_audio audio/french_1_trimmed.wav \
+                    --output_dir outputs/talkshow_french/RLW104 \
+                    --num_samples 3
+
+python inference.py --person_id TXB805 \
+                    --input_audio audio/french_1_trimmed.wav \
+                    --output_dir outputs/talkshow_french/TXB805 \
+                    --num_samples 3
+
+                    
